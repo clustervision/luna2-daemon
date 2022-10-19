9e04d700b43593d0925d094a20599917d4ac3f90
Sumit Sharma <sumitsharma42876@gmail.com>
2022-10-19 14:09:53 +0800

    TRIX-38 Strategy Change: Service File Ready

--- config/server.sh
+++ config/server.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 # This File should be called via systemd service to start/stop the Luna 2 Daemon Application.
-# INI Check; Sanity Checks
 
 cd /trinity/local/luna/
-# nohup /usr/local/bin/python3.10 luna.py 2>&1 > /dev/null &
-gunicorn -w 4 -b 0.0.0.0:7050 --log-file ./log/gunicorn.log --log-level INFO --access-logfile ./log/gunicorn-access.log --error-logfile ./log/gunicorn-error.log 'luna:api' &
-# nohup gunicorn -w 4 -b 0.0.0.0:7050 'luna:api' &
+nohup gunicorn --config /trinity/local/luna/config/server.py 'luna:api' &

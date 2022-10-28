a186fe34c72470c360120f5e72293136580a8573
Sumit Sharma <sumitsharma42876@gmail.com>
2022-10-28 11:08:31 +0800

    TRIX-39 Add tempfile, templates, dir in boot API

--- apis/boot.py
+++ apis/boot.py
@@ -17,7 +17,7 @@ from flask import Blueprint, request, json, render_template
 from utils.log import *
 
 logger = Log.get_logger()
-boot_blueprint = Blueprint('boot', __name__, template_folder='../ipxe') # , template_folder='templates'
+boot_blueprint = Blueprint('boot', __name__, template_folder='../templates') # , template_folder='templates'
 
 
 """
--- config/server.sh
+++ config/server.sh
@@ -3,4 +3,4 @@
 # This File should be called via systemd service to start/stop the Luna 2 Daemon Application.
 
 cd /trinity/local/luna/
-nohup gunicorn --config /trinity/local/luna/config/server.py 'luna:api' &
+nohup gunicorn --config /trinity/local/luna/config/gunicorn.py 'luna:api' &

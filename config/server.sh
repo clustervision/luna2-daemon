edb7d4f3955b60cc39082609cb47983709bd3293
Sumit Sharma <sumitsharma42876@gmail.com>
2022-11-01 19:14:51 +0800

    TRIX-39 WIP gunicorn reload

--- config/server.sh
+++ config/server.sh
@@ -3,4 +3,10 @@
 # This File should be called via systemd service to start/stop the Luna 2 Daemon Application.
 
 cd /trinity/local/luna/
-nohup gunicorn --config /trinity/local/luna/config/gunicorn.py 'luna:api' &
+if [[ "$1" = "reload" ]]; then
+	gunicorn --config /trinity/local/luna/config/gunicorn.py --reload 'luna:api' &
+else
+	gunicorn --config /trinity/local/luna/config/gunicorn.py 'luna:api' &
+fi  
+# nohup gunicorn --config /trinity/local/luna/config/gunicorn.py 'luna:api' >/dev/null 2>&1
+# nohup gunicorn --config /trinity/local/luna/config/gunicorn.py 'luna:api' &

b9cd11ba95ca3f94e62b5375929434ed874d61a1
Sumit Sharma <sumitsharma42876@gmail.com>
2022-12-01 16:35:29 +0800

    Linting & Solving Issues

--- utils/files.py
+++ utils/files.py
@@ -1,57 +1,68 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 """
 Files Class is reponsible to validate the TARBALL directory and it's files.
 It can provvide the list of file and a specific file.
-It can only provide files ending with tar.gz and tar.bz2
-
+It can only provide files ending with tar.gz and tar.bz2.
 """
 
-__author__      = "Sumit Sharma"
-__copyright__   = "Copyright 2022, Luna2 Project"
-__license__     = "GPL"
-__version__     = "2.0"
-__maintainer__  = "Sumit Sharma"
-__email__       = "sumit.sharma@clustervision.com"
-__status__      = "Development"
-
+__author__      = 'Sumit Sharma'
+__copyright__   = 'Copyright 2022, Luna2 Project'
+__license__     = 'GPL'
+__version__     = '2.0'
+__maintainer__  = 'Sumit Sharma'
+__email__       = 'sumit.sharma@clustervision.com'
+__status__      = 'Development'
 
 import os
-from utils.log import *
-logger = Log.get_logger()
-
+from utils.log import Log
+from common.constant import CONSTANT
 
 class Files(object):
     """
-    TODO: add docs
+    Files Class take care all Operations related to files.
     """
+
     def __init__(self):
-        pass
+        """
+        Constructor - For Files Operations.
+        """
+        self.logger = Log.get_logger()
 
-    """
-    Input - filename
-    Process - It will check if file is available or not 
-    Output - filepath
-    """
+    
     def check_file(self, filename):
-        filepath = CONSTANT["FILES"]["TARBALL"]+"/"+filename
-        if os.path.exists(filepath):
-            logger.debug("File Path {} Is exists.".format(filepath))
-            return filepath
-        else:
-            logger.debug("File Path {} Is Not exists.".format(filepath))
-            return False
+        """
+        Input - filename
+        Process - It will check if file is available or not 
+        Output - filepath
+        """
+        filepath = f'{CONSTANT["FILES"]["TARBALL"]}/{filename}'
+        try:
+            if os.path.exists(filepath):
+                self.logger.debug(f'File Path {filepath} Is exists.')
+                response = filepath
+        except FileNotFoundError:
+            self.logger.debug(f'File Path {filepath} Is Not exists.')
+            response = False
+        return response
 
 
-    """
-    Input - None
-    Process - It will collect *tar.gz and *tar.bz2 files from TARBALL directory
-    Output - List Of Available Files
-    """
     def list_files(self):
+        """
+        Input - None
+        Process - It will collect *tar.gz and *tar.bz2 files from TARBALL directory
+        Output - List Of Available Files.
+        """
         files = []
-        filepath = CONSTANT["FILES"]["TARBALL"]+"/"
-        if os.path.exists(filepath):
-            for file in os.listdir(filepath):
-                if file.endswith(".tar.gz") or file.endswith(".tar.bz2"):
-                    files.append(file)
-        return files
+        filepath = f'{CONSTANT["FILES"]["TARBALL"]}/'
+        try:
+            if os.path.exists(filepath):
+                for file in os.listdir(filepath):
+                    if file.endswith('.tar.gz') or file.endswith('.tar.bz2'):
+                        files.append(file)
+                        response = file
+        except FileNotFoundError:
+            self.logger.debug(f'File Path {filepath} Is Not exists.')
+            response = None
+        return response

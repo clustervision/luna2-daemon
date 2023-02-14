e4334936e73ccb2d8a25386f286adbe08f1c25d1
root <bart.lamboo@clustervision.com>
2023-02-14 10:34:15 -0500

    NOJIRA code cleanup

--- utils/database.py
+++ utils/database.py
@@ -102,6 +102,13 @@ class Database(object):
             response = None
         return response
 
+    def convert_string_to_list(self,myitem):
+        if type(myitem) == type('string'):
+            mylist=[]
+            mylist.append(str(myitem))
+        else:
+            mylist=myitem
+        return mylist
 
     def get_record_join(self, select=None, joinon=None, where=None):
         """
@@ -112,13 +119,13 @@ class Database(object):
                     where can be None OR complete where condition
         Output - Fetch rows along with column name.
         """
+
+
+
         if select:
             #strcolumn = ','.join(map(str, select))
             cols=[]
-            if type(select) == type('string'):
-                t=select
-                select=[]
-                select.append(str(t))
+            select=self.convert_string_to_list(select)
             for eachselect in select:
                 table,col=eachselect.split('.',1)
                 #str_output = re.sub(regex_search_term, regex_replacement, str_input)
@@ -131,16 +138,15 @@ class Database(object):
         else:
             strcolumn = "*"
         if joinon:
-            #strjoin = ' AND '.join(map(str, joinon))
-            if type(joinon) == type('string'):
-                t=joinon
-                joinon=[]
-                joinon.append(str(t))
+            """
+            in here we do two things. we disect the joins, polish them (`) and we gather the involved tables
+            """
+            joinon=self.convert_string_to_list(joinon)
             joins=[]
             tables=[]
             for eachjoin in joinon:
                 left,right=eachjoin.split('=')
-                print(f" ----> [{left}], [{right}]")
+#                print(f" ----> [{left}], [{right}]")
                 lefttable,leftcol=left.split('.')
                 righttable,rightcol=right.split('.')
                 joins.append(f"`{lefttable}`.{leftcol}=`{righttable}`.{rightcol}")
@@ -148,19 +154,16 @@ class Database(object):
                     tables.append(lefttable)
                 if righttable not in tables:
                     tables.append(righttable)
-            print(tables)
+            #print(tables)
             #tablestr = ','.join(joinon.map(lambda x:x.split('.',1)[0])) #    map(lambda x:x.split('.', 1)[0])
             tablestr = '`,`'.join(tables)
             strjoin = ' AND '.join(joins)
             print(f"{strjoin}")
-        else:
+        else: # no join? we give up. this function is called _join so you better specify one
             response = None
             return response
         if where:
-            if type(where) == type('string'):
-                t=where
-                where=[]
-                where.append(str(t))
+            where=self.convert_string_to_list(where)
             strwhere = ' AND '.join(map(str, where))
         if where and joinon:
             query = f'SELECT {strcolumn} FROM `{tablestr}` WHERE {strjoin} AND {strwhere};'
@@ -171,6 +174,33 @@ class Database(object):
             return response
         self.logger.debug(f'Query executing => {query}.')
         print(f'Query executing => {query}.')
+        try:
+            self.cursor.execute(query)
+            names = list(map(lambda x: x[0], self.cursor.description)) # Fetching the Column Names
+            data = self.cursor.fetchall()
+            self.logger.debug(f'Dataset retrived => {data}.')
+            rowdict = {}
+            response = []
+            for row in data:
+                for key, value in zip(names,row):
+                    rowdict[key] = value
+                response.append(rowdict)
+                rowdict = {}
+        except pyodbc.Error as exp:
+            self.logger.error(f'Error occur while executing => {query}. error is {exp}.')
+            response = None
+        return response
+
+    def get_record_query(self, query=None):
+        """
+        Input - Complete SQL query with Joins
+        Process - It is SELECT operation on the DB.
+                    select can be comma separated column name or None.
+                    table is the table name where the select operation should be happen.
+                    where can be None OR complete where condition
+        Output - Fetch rows along with column name.
+        """
+        self.logger.debug(f'Query executing => {query}.')
         try:
             self.cursor.execute(query)
             names = list(map(lambda x: x[0], self.cursor.description)) # Fetching the Column Names

07f9b5e920153a098a177d1e45c8aba179ea7979
Antoine <antoine.schonewille@clustervision.com>
2023-08-18 16:55:28 +0200

    NOTRIX found an interesting potential bug where an empty osrelease will not load the default in levelone, but None

--- daemon/utils/helper.py
+++ daemon/utils/helper.py
@@ -742,7 +742,7 @@ class Helper(object):
         """
         self.logger.debug(f"Loading module {class_name}/Plugin from plugins.{root}.{levelone}.{leveltwo} / {plugins}")
         if (not plugins) or (root and root not in plugins):
-            self.logger.error("Provided Plugins tree is empty or is missing root. plugins = [{plugins}], root = [{root}]")
+            self.logger.error(f"Provided Plugins tree is empty or is missing root. plugins = [{plugins}], root = [{root}]")
             return None
         module = None
         class_name = class_name or 'Plugin'

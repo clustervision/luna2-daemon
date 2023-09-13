8d1db84f34bbfa2b69fcf6369e55ada60333541a
Antoine <antoine.schonewille@clustervision.com>
2023-09-13 18:45:32 +0200

    TRIX-147 more steps. adding plugins

--- daemon/common/bootstrap.py
+++ daemon/common/bootstrap.py
@@ -221,6 +221,7 @@ def create_database_tables():
     Database().create("status", DATABASE_LAYOUT_status)
     Database().create("queue", DATABASE_LAYOUT_queue)
     Database().create("osimage", DATABASE_LAYOUT_osimage)
+    Database().create("osimagetag", DATABASE_LAYOUT_osimagetag)
     Database().create("nodesecrets", DATABASE_LAYOUT_nodesecrets)
     Database().create("nodeinterface", DATABASE_LAYOUT_nodeinterface)
     Database().create("bmcsetup", DATABASE_LAYOUT_bmcsetup)

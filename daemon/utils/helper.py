66dfcb55bc68518714c3539b2f6eafcf3535cf9d
Antoine <antoine.schonewille@clustervision.com>
2023-09-13 13:51:03 +0200

    TRIX-147 more steps. adding plugins

--- daemon/utils/helper.py
+++ daemon/utils/helper.py
@@ -740,9 +740,7 @@ class Helper(object):
         """
         This method will load the plugin.
         """
-        if root:
-            root = root.replace('/','.')
-        roottree = root.split('\.')
+        roottree = root.split('/')
         self.logger.info(f"Loading module {class_name}/Plugin from plugins.{root}.{levelone}.{leveltwo} / {plugins}")
         if (not plugins): # or (root and root not in plugins):
             self.logger.error(f"Provided Plugins tree is empty or is missing root. plugins = [{plugins}], root = [{root}]")

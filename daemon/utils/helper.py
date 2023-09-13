5d909324258c078d7d23e34471289e305de87a36
Antoine <antoine.schonewille@clustervision.com>
2023-09-13 14:32:26 +0200

    TRIX-147 more steps. adding plugins

--- daemon/utils/helper.py
+++ daemon/utils/helper.py
@@ -789,7 +789,7 @@ class Helper(object):
         except Exception as exp:
             exc_type, exc_obj, exc_tb = sys.exc_info()
             #fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-            print(exc_type, fname, exc_tb.tb_lineno)
+            #print(exc_type, fname, exc_tb.tb_lineno)
             self.logger.error(f"Loading module caused a problem during selection: {exp}, {exc_type} in {exc_tb.tb_lineno}. myplugin: [{myplugin}]") 
             return None
 

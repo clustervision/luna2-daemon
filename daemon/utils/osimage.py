2abe4705d2901567135c5616ec605ac54863b2ed
antoine <antoine.schonewille@clustervision.com>
2023-11-24 14:04:43 +0100

    TRIX-146 adding cleanup of old images for slave masters

--- daemon/utils/osimage.py
+++ daemon/utils/osimage.py
@@ -725,12 +725,12 @@ class OsImage(object):
         image = Database().get_record(None, 'osimage', f"WHERE name='{osimage}'")
         if image:
             for item in ['kernelfile','initrdfile','imagefile']:
-            if image[0][item]:
-                inusebytag = Database().get_record(None, 'osimagetag', f"WHERE osimageid='{image[0]['id']}' AND {item}='"+image[0][item]+"'")
-                if not inusebytag:
-                    queue_id,queue_response = Queue().add_task_to_queue(f'cleanup_old_file:'+image[0][item],'housekeeper',request_id,None,'1h')
-                    if item == 'imagefile':
-                        queue_id,queue_response = Queue().add_task_to_queue(f'cleanup_old_provisioning:'+image[0][item],'housekeeper',request_id,None,'1h')
+                if image[0][item]:
+                    inusebytag = Database().get_record(None, 'osimagetag', f"WHERE osimageid='{image[0]['id']}' AND {item}='"+image[0][item]+"'")
+                    if not inusebytag:
+                        queue_id,queue_response = Queue().add_task_to_queue(f'cleanup_old_file:'+image[0][item],'housekeeper',request_id,None,'1h')
+                        if item == 'imagefile':
+                            queue_id,queue_response = Queue().add_task_to_queue(f'cleanup_old_provisioning:'+image[0][item],'housekeeper',request_id,None,'1h')
 
     # ------------------------------------------------------------------- 
     # The mother of all.

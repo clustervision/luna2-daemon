6310c84b80dccb2f2c437662f139f067c122ddc4
antoine <antoine.schonewille@clustervision.com>
2024-06-12 17:42:33 +0200

    TRIX-537 /run/* added as osgrab exclude

--- daemon/base/osimage.py
+++ daemon/base/osimage.py
@@ -238,7 +238,7 @@ class OSImage():
         # things we have to set for a group
         items = {
             'grab_filesystems': '/, /boot',
-            'grab_exclude': '/proc/*, /sys/*, /dev/*, /tmp/*, /var/log/*',
+            'grab_exclude': '/proc/*, /sys/*, /dev/*, /tmp/*, /run/*, /var/log/*',
             'kernelmodules': 'ipmi_devintf, ipmi_si, ipmi_msghandler'
         }
         if request_data:
--- daemon/common/bootstrap.py
+++ daemon/common/bootstrap.py
@@ -530,7 +530,7 @@ def bootstrap(bootstrapfile=None):
     default_osimage = [
         {'column': 'name', 'value': str(BOOTSTRAP['OSIMAGE']['NAME'])},
         {'column': 'grab_filesystems', 'value': '/, /boot'},
-        {'column': 'grab_exclude', 'value': '/proc/*, /sys/*, /dev/*, /tmp/*, /var/log/*'},
+        {'column': 'grab_exclude', 'value': '/proc/*, /sys/*, /dev/*, /tmp/*, /run/*, /var/log/*'},
         {'column': 'kernelversion', 'value': f'{osimage_kernelversion}'},
         {'column': 'kerneloptions', 'value': 'net.ifnames=0 biosdevname=0'},
         {'column': 'path', 'value': f'{osimage_path}'},
@@ -544,7 +544,7 @@ def bootstrap(bootstrapfile=None):
 #    ubuntu_osimage = [
 #        {'column': 'name', 'value': 'ubuntu'},
 #        {'column': 'grab_filesystems', 'value': '/, /boot'},
-#        {'column': 'grab_exclude', 'value': '/proc/*, /sys/*, /dev/*, /tmp/*, /var/log/*'},
+#        {'column': 'grab_exclude', 'value': '/proc/*, /sys/*, /dev/*, /tmp/*, /run/*, /var/log/*'},
 #        {'column': 'kernelversion', 'value': ''},
 #        {'column': 'path', 'value': f'{ubuntu_path}'},
 #        {'column': 'kernelmodules', 'value': 'ipmi_devintf, ipmi_si, ipmi_msghandler'},

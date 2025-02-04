d231f5dd89cf4388c644d1d0d5fca5f2fe25836c
Diego Sonaglia <diego.sonaglia@clustervision.com>
2025-02-04 05:04:35 -0500

    NOTRIX remove configs overrides from development

--- daemon/config/gunicorn.py
+++ daemon/config/gunicorn.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python3
 
+# This code is part of the TrinityX software suite
+# Copyright (C) 2023  ClusterVision Solutions b.v.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>
+
 """
 Start Luna 2 Daemon as a service luna2-daemon.service
 """
 import luna
 # The IP address (typically localhost) and port that the gunicorn WSGI process should listen on
-bind = '0.0.0.0:7050'
+bind = '[::]:7050'
+#bind = '0.0.0.0:7050'
 
 # Number of gunicorn workers to spawn. This should typically be 2n+1, where
 # n is the number of CPU cores present.
@@ -15,7 +32,7 @@ workers = 4
 # threads = 3
 
 # Timeout (in seconds) for a request to complete
-timeout = 600
+# timeout = 120
 
 # The maximum number of requests a worker can handle before being spawned
 # max_requests = 5000
@@ -29,7 +46,3 @@ on_exit = luna.on_exit              ## Called when service exits
 on_reload = luna.on_reload          ## Called when service reload
 worker_abort = luna.worker_abort    ## Called when a worker is killed
 #### Automated Process ####
-
-certfile = "/trinity/local/etc/ssl/office-controller1.cluster.crt"
-keyfile = "/trinity/local/etc/ssl/office-controller1.cluster.key"
-cert_reqs = 0

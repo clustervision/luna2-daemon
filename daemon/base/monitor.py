148ace98b52c7aebd64cead7c7a642956096f743
antoine <antoine.schonewille@clustervision.com>
2025-09-19 14:16:32 +0200

    NOJIRA removing prepend for context full messages

--- daemon/base/monitor.py
+++ daemon/base/monitor.py
@@ -140,11 +140,14 @@ class Monitor():
         tablerefid = 0
         tablename = item
         tableref = ''
+        prepend = True
         if item in ['ha', 'mother']:
             tablename = 'reference'
             tableref = 'tableref'
+            prepend = False
         elif item == 'sync':
             tablename = 'osimage'
+            prepend = False
         db_items = Database().get_record_join([f'{tablename}.{tableref}name','monitor.state','monitor.status'],
                                               [f'monitor.tablerefid={tablename}.id'],
                                               [f"monitor.tableref='{item}'"])
@@ -155,8 +158,12 @@ class Monitor():
                 if item == "node":
                     _,servicestatus=monitor().installer_state(db_item['state'],db_item['status'])
                 state = db_item['state'] or "ok"
+                if prepend:
+                    object_name = db_item[tableref+'name']+" "
+                else:
+                    object_name = ""
                 response['monitor']['status'][item][db_item[tableref+'name']] = {
-                    "state": db_item[tableref+'name']+" "+state,
+                    "state": object_name+state,
                     "status": f"{servicestatus}"}
         else:
             response = None

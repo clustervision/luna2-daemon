494af81d057e3d75144cc8e4a8f792b4f5ba1956
mila <mila@clustervision.com>
2026-05-14 14:51:34 +0200

    TRIX-1252 port EFI/rootmnt improvements to raid1.py

--- daemon/plugins/boot/scripts/raid1.py
+++ daemon/plugins/boot/scripts/raid1.py
@@ -155,22 +155,22 @@ if [ "$FORMAT_MY_DISK" == "yes" ]; then
         swaplabel -L swappart ${MY_LOCAL_DISK2_NAME}${DP2}3
 fi
 echo "*** RAID1 script: mounting disks"
-umount -l /sysroot &> /dev/null
-mount /dev/md0 /sysroot
-mkdir /sysroot/boot
-mount ${MY_LOCAL_DISK1_NAME}${DP1}2 /sysroot/boot
-mkdir /sysroot/boot/efi
-mount ${MY_LOCAL_DISK1_NAME}${DP1}1 /sysroot/boot/efi
+umount -l "$rootmnt" &> /dev/null
+mount /dev/md0 "$rootmnt"
+mkdir "$rootmnt"/boot
+mount ${MY_LOCAL_DISK1_NAME}${DP1}2 "$rootmnt"/boot
+mkdir "$rootmnt"/boot/efi
+mount ${MY_LOCAL_DISK1_NAME}${DP1}1 "$rootmnt"/boot/efi
     """
 
     postscript = """
 . /tmp/my-local-disk.sh
 . /tmp/my-fetch-disk.sh
 
-mkdir /sysroot/proc /sysroot/dev /sysroot/sys &> /dev/null
-mount -t proc proc /sysroot/proc 
-mount -t devtmpfs devtmpfs /sysroot/dev
-mount -t sysfs sysfs /sysroot/sys
+mkdir "$rootmnt"/proc "$rootmnt"/dev "$rootmnt"/sys &> /dev/null
+mount -t proc proc "$rootmnt"/proc 
+mount -t devtmpfs devtmpfs "$rootmnt"/dev
+mount -t sysfs sysfs "$rootmnt"/sys
 
 echo "*** RAID1 script: writing fstab"
 FSTAB_DISK1=${MY_LOCAL_DISK1_NAME}
@@ -194,10 +194,10 @@ FSTAB_DISK2_P3=$(blkid -o export ${FSTAB_DISK2}${FSTAB_DP2}3 | grep -w UUID || e
 
 grep -v -e md0 -e "${FSTAB_DISK1}" -e "${FSTAB_DISK2}" \
     -e "${FSTAB_MD0}" -e "${FSTAB_DISK1_P1}" -e "${FSTAB_DISK1_P2}" \
-    -e "${FSTAB_DISK1_P3}" -e "${FSTAB_DISK2_P3}" /sysroot/etc/fstab > /tmp/fstab
-grep -v -w '/' /tmp/fstab > /sysroot/etc/fstab
+    -e "${FSTAB_DISK1_P3}" -e "${FSTAB_DISK2_P3}" "$rootmnt"/etc/fstab > /tmp/fstab
+grep -v -w '/' /tmp/fstab > "$rootmnt"/etc/fstab
 
-cat << EOF >> /sysroot/etc/fstab
+cat << EOF >> "$rootmnt"/etc/fstab
 ${FSTAB_MD0}   /       ext4    defaults        1 1
 ${FSTAB_DISK1_P2}   /boot   ext4    defaults        1 2
 ${FSTAB_DISK1_P1}   /boot/efi   vfat    defaults        1 2
@@ -207,38 +207,97 @@ EOF
 
 if [ "$MAKE_BOOT" == "yes" ]; then
     echo "*** RAID1 script: building new ramdisk"
-    echo "AUTO -all" > /sysroot/etc/mdadm.conf
-    chroot /sysroot /bin/bash -c "mdadm --detail --scan --verbose | grep ARRAY >> /etc/mdadm.conf"
+    echo "AUTO -all" > "$rootmnt"/etc/mdadm.conf
+    chroot "$rootmnt" /bin/bash -c "mdadm --detail --scan --verbose | grep ARRAY >> /etc/mdadm.conf"
 
-    rm -rf /sysroot/lib/dracut/modules.d/95luna/
-    chroot /sysroot /bin/bash -c "dracut -f --mdadmconf --add=mdraid --add-drivers='raid1'"
+    rm -rf "$rootmnt"/lib/dracut/modules.d/95luna/
+    CHROOT_INITRAMFS=$(chroot "$rootmnt" /bin/bash -c 'if command -v dracut >/dev/null 2>&1; then echo dracut; elif command -v update-initramfs >/dev/null 2>&1; then echo update-initramfs; fi')
+    if [ "$CHROOT_INITRAMFS" == "dracut" ]; then
+        chroot "$rootmnt" /bin/bash -c "dracut -f --mdadmconf --add=mdraid --add-drivers='raid1'"
+    elif [ "$CHROOT_INITRAMFS" == "update-initramfs" ]; then
+        chroot "$rootmnt" /bin/bash -c "update-initramfs -u"
+    fi
 
-    echo "*** RAID1 script: configuring shim boot partition"
-    GRUB_CMDLINE_LINUX=$(grep GRUB_CMDLINE_LINUX /sysroot/etc/default/grub || echo 'GRUB_CMDLINE_LINUX=')
+    echo "*** RAID1 script: configuring grub/shim boot partition"
+    GRUB_CMDLINE_LINUX=$(grep GRUB_CMDLINE_LINUX "$rootmnt"/etc/default/grub || echo 'GRUB_CMDLINE_LINUX=')
     GRUB_CMDLINE_LINUX=$(echo $GRUB_CMDLINE_LINUX | tr -d '"')
     GRUB_CMDLINE_LINUX="${GRUB_CMDLINE_LINUX} rd.md=1 rd.md.conf=1 rd.auto=1 net.ifnames=1 biosdevname=0"
     GRUB_CMDLINE_LINUX=$(echo $GRUB_CMDLINE_LINUX | sed -e 's/GRUB_CMDLINE_LINUX=/GRUB_CMDLINE_LINUX="/' | sed -e 's/$/"/')
-    grep -v GRUB_CMDLINE_LINUX /sysroot/etc/default/grub > /tmp/grub-def
-    cat /tmp/grub-def > /sysroot/etc/default/grub
-    echo "$GRUB_CMDLINE_LINUX" >> /sysroot/etc/default/grub
+    grep -v GRUB_CMDLINE_LINUX "$rootmnt"/etc/default/grub > /tmp/grub-def
+    cat /tmp/grub-def > "$rootmnt"/etc/default/grub
+    echo "$GRUB_CMDLINE_LINUX" >> "$rootmnt"/etc/default/grub
 
-    SH=`chroot /sysroot /bin/bash -c "efibootmgr -v|grep Shim1|grep -oE '^Boot[0-9]+'|grep -oE '[0-9]+'"`
-    if [ "$SH" ]; then
-        chroot /sysroot /bin/bash -c "efibootmgr -B -b $SH"
+    OS_ID=$(chroot "$rootmnt" /bin/bash -c '. /etc/os-release >/dev/null 2>&1; echo $ID')
+    DISTRO=$(ls "$rootmnt"/boot/efi/EFI/ 2>/dev/null | grep -im1 -e ubuntu -e rocky -e redhat -e alma -e centos)
+    if [ ! "$DISTRO" ]; then
+        case "$OS_ID" in
+            ubuntu|rocky|redhat|alma|centos) DISTRO=$OS_ID ;;
+            *) DISTRO=rocky ;;
+        esac
     fi
-    DISTRO=$(ls /sysroot/boot/efi/EFI/ | grep -ie rocky -e redhat -e alma -e centos || echo rocky)
-    chroot /sysroot /bin/bash -c "efibootmgr --disk ${MY_LOCAL_DISK1_NAME}${DP1} --part 1 --create --label \"Shim1\" --loader /EFI/${DISTRO}/shimx64.efi; \
-                                  grub2-mkconfig -o /boot/efi/EFI/${DISTRO}/grub.cfg"
+
+    EFI_TARGET=x86_64-efi
+    EFI_SHIM=shimx64.efi
+    EFI_GRUB=grubx64.efi
+    EFI_BOOT=BOOTX64.EFI
+    if chroot "$rootmnt" /bin/bash -c "[ -e /usr/lib/shim/shimaa64.efi.signed ] || uname -m | grep -Eq '^(aarch64|arm64)$'"; then
+        EFI_TARGET=arm64-efi
+        EFI_SHIM=shimaa64.efi
+        EFI_GRUB=grubaa64.efi
+        EFI_BOOT=BOOTAA64.EFI
+    fi
+
+    CHROOT_GRUB_INSTALL=$(chroot "$rootmnt" /bin/bash -c 'if command -v grub2-install >/dev/null 2>&1; then echo grub2-install; elif command -v grub-install >/dev/null 2>&1; then echo grub-install; fi')
+    CHROOT_GRUB_MKCONFIG=$(chroot "$rootmnt" /bin/bash -c 'if command -v grub2-mkconfig >/dev/null 2>&1; then echo grub2-mkconfig; elif command -v grub-mkconfig >/dev/null 2>&1; then echo grub-mkconfig; fi')
+
+    EFI_CFG=/boot/efi/EFI/${DISTRO}/grub.cfg
+    BOOT_COPY_DIR=""
+    if [ "$OS_ID" == "ubuntu" ]; then
+        DISTRO=ubuntu
+        EFI_CFG=/boot/grub/grub.cfg
+        if [ "$EFI_TARGET" == "arm64-efi" ]; then
+            BOOT_COPY_DIR="$rootmnt/boot/efi/EFI/BOOT"
+        fi
+    fi
+
+    if [ "$CHROOT_GRUB_INSTALL" ]; then
+        echo "*** RAID1 script: installing ${DISTRO} EFI bootloader [$EFI_TARGET]"
+        for EXTRA in '' '--removable'; do
+            chroot "$rootmnt" /bin/bash -c "$CHROOT_GRUB_INSTALL --target=${EFI_TARGET} --efi-directory=/boot/efi --bootloader-id=${DISTRO} ${EXTRA} --no-nvram"
+        done
+    fi
+
+    if [ "$BOOT_COPY_DIR" ]; then
+        mkdir -p "$BOOT_COPY_DIR"
+        [ -e "$rootmnt"/boot/efi/EFI/${DISTRO}/${EFI_SHIM} ] && cp -f "$rootmnt"/boot/efi/EFI/${DISTRO}/${EFI_SHIM} "$BOOT_COPY_DIR/${EFI_BOOT}"
+        [ -e "$rootmnt"/boot/efi/EFI/${DISTRO}/${EFI_GRUB} ] && cp -f "$rootmnt"/boot/efi/EFI/${DISTRO}/${EFI_GRUB} "$BOOT_COPY_DIR/${EFI_GRUB}"
+    fi
+
+    EFI_LOADER=$EFI_SHIM
+    [ ! -e "$rootmnt"/boot/efi/EFI/${DISTRO}/${EFI_LOADER} ] && EFI_LOADER=$EFI_GRUB
+
+    if [ -e "$rootmnt"/boot/efi/EFI/${DISTRO}/${EFI_LOADER} ]; then
+        BOOT_DISK=$(readlink -f ${MY_LOCAL_DISK1_NAME} 2>/dev/null || echo ${MY_LOCAL_DISK1_NAME})
+        SH=$(chroot "$rootmnt" /bin/bash -c "efibootmgr -v 2>/dev/null|grep Shim1|grep -oE '^Boot[0-9]+'|grep -oE '[0-9]+'")
+        [ "$SH" ] && chroot "$rootmnt" /bin/bash -c "efibootmgr -B -b $SH"
+        chroot "$rootmnt" /bin/bash -c "if command -v efibootmgr >/dev/null 2>&1 && efibootmgr -v >/dev/null 2>&1; then efibootmgr --disk "${BOOT_DISK}" --part 1 --create --label "Shim1" --loader /EFI/${DISTRO}/${EFI_LOADER}; else echo '*** RAID1 script: efibootmgr unavailable or EFI vars inaccessible, relying on fallback bootloader'; fi"
+    fi
+
+    if [ "$CHROOT_GRUB_MKCONFIG" ]; then
+        chroot "$rootmnt" /bin/bash -c "$CHROOT_GRUB_MKCONFIG -o ${EFI_CFG}"
+    fi
+    if [ "$BOOT_COPY_DIR" ] && [ -e "$rootmnt${EFI_CFG}" ]; then
+        cp -f "$rootmnt${EFI_CFG}" "$BOOT_COPY_DIR/grub.cfg"
+    fi
     # commented out next command as it imposes reboots. When netboot is set to no and with correct bios settings,
     # this would impose desired behavior. To cover all bases, we now relabel before the pivot. See below.
-    #$null > /sysroot/.autorelabel
+    #$null > "$rootmnt"/.autorelabel
 fi
 
-chroot /sysroot /bin/bash -c "cd /boot && ln -s /boot boot; \
-                              restorecon -r -p / 2> /dev/null"
+chroot "$rootmnt" /bin/bash -c "cd /boot && ln -s /boot boot; restorecon -r -p / 2> /dev/null"
 
-umount /sysroot/sys
-umount /sysroot/dev
-umount /sysroot/proc
+umount "$rootmnt"/sys
+umount "$rootmnt"/dev
+umount "$rootmnt"/proc
 echo "*** RAID1 script: done"
     """
